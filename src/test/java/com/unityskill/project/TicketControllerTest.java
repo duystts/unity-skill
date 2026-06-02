17475883c512996e54cf7faa76fcbcfb65c9b68b
ngohung55235-cmd <ngohung55235@gmail.com>
2026-06-02 21:08:43 +0700

    fix: add missing tags param to TicketResponse in test constructors

--- src/test/java/com/unityskill/project/TicketControllerTest.java
+++ src/test/java/com/unityskill/project/TicketControllerTest.java
@@ -52,7 +52,8 @@ class TicketControllerTest {
             false,                         // hasPr
             null,                          // closedAt
             "2026-01-01T00:00:00Z",        // createdAt
-            "2026-01-01T00:00:00Z"         // updatedAt
+            "2026-01-01T00:00:00Z",        // updatedAt
+            List.of()                      // tags
         );
     }
 
